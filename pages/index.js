fee2c1e313d25e14da9ca4ba548db8073920db7d
jaky V <atomic.jacopo@gmail.com>
2023-08-23 00:03:08 +0200

    inizio del lavoro sul progetto

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react'; 
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) => {
+    console.log(event.target.value); 
+    setUserInput(event.target.value); 
+  }
+
   return (
     <div className="root">
       <Head>
@@ -11,17 +18,40 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>sup, lets make it EASY ! </h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>make your knowledge flow, make everything undestandable and summarized  </h2>
           </div>
+           <div className="header-subtitle">
+            <h2>I was born not knowing and have had only a little time to change that here and there.
+Richard P. Feynman </h2>
+          </div>
+
+         <div className="prompt-container">
+  <textarea
+    placeholder="start typing here"
+    className="prompt-box"
+    value={userInput}
+    onChange={onUserChangedText}
+  />
+  {/* New code I added here */}
+  <div className="prompt-buttons">
+    <a className="generate-button" onClick={null}>
+      <div className="generate">
+        <p>Generate</p>
+      </div>
+    </a>
+  </div>
+</div>
+
+
+
         </div>
       </div>
       <div className="badge-container grow">
         <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
+          href="https://buildspace.so/builds/ai-writer" target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
